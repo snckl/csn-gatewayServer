313f4b080762aa82e9de28777613c00dfe2c33ae
Sinan <sinan.cakal00@gmail.com>
2024-04-03 15:25:19 +0300

    Added retry pattern for comment and storage ms

--- src/main/java/com/csn/gatewayserver/GatewayServerApplication.java
+++ src/main/java/com/csn/gatewayserver/GatewayServerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -20,20 +22,26 @@ public class GatewayServerApplication {
 		return routeLocatorBuilder.routes()
 				.route(p -> p
 						.path("/csn/post/**") // Specifies for any request whose	path starts with this will apply.
-						.filters(f -> f.rewritePath("/csn/post/(?<segment>.*)","/${segment}")
+						.filters(f -> f.rewritePath("/csn/post/(?<segment>.*)","/${segment}") // rewrite path.
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.circuitBreaker(config -> config.setName("postCircuitBreaker")
-										.setFallbackUri("forward:/contactSupport"))) // rewrite path.
+										.setFallbackUri("forward:/contactSupport")))
 						.uri("lb://POST-SERVICE")) // forwarding to ms named as POST-SERVICE in eureka
 				.route(p -> p
 						.path("/csn/comment-service/**")
 						.filters(f -> f.rewritePath("/csn/comment/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig -> retryConfig.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)))
 						.uri("lb://COMMENT-SERVICE"))
 				.route(p -> p
 						.path("/csn/storage-service/**")
 						.filters(f -> f.rewritePath("/csn/storage/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig -> retryConfig.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)))
 						.uri("lb://STORAGE-SERVICE")).build();
 	}
 
